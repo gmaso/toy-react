ae60102635b492bb1ef9b15e2f9cda0945d5381e
gmaso <gmaso87@163.com>
2020-10-22 22:12:31 +0800

    createElement

--- dist/main.js
+++ dist/main.js
@@ -12,6 +12,6 @@
   \*****************/
 /*! unknown exports (runtime-defined) */
 /*! runtime requirements:  */
-eval("for (var _i = 0, _arr = [1, 2, 3]; _i < _arr.length; _i++) {\n  var i = _arr[_i];\n  console.log(i);\n}\n\nvar a = createElement(\"div\", null);\n\n//# sourceURL=webpack://toy-react/./main.js?");
+eval("for (var _i = 0, _arr = [1, 2, 3]; _i < _arr.length; _i++) {\n  var i = _arr[_i];\n  console.log(i);\n}\n\nfunction createElement(tagName, attributes) {\n  var tag = document.createElement(tagName);\n\n  if (attributes) {\n    for (var attr in attributes) {\n      tag.setAttribute(attr, attributes[attr]);\n    }\n  }\n\n  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {\n    children[_key - 2] = arguments[_key];\n  }\n\n  if (children) {\n    for (var _i2 in children) {\n      var child = children[_i2];\n      tag.appendChild(child);\n    }\n  }\n\n  return tag;\n}\n\nwindow.a = createElement(\"div\", {\n  id: \"a\",\n  \"class\": \"c\"\n}, createElement(\"div\", null), createElement(\"div\", null), createElement(\"div\", null));\n\n//# sourceURL=webpack://toy-react/./main.js?");
 /******/ })()
 ;
--- main.js
+++ main.js
@@ -2,4 +2,24 @@ for (let i of [1, 2, 3]) {
   console.log(i);
 }
 
-let a = <div/>
+function createElement(tagName, attributes, ...children) {
+  let tag = document.createElement(tagName);
+  if (attributes) {
+    for (let attr in attributes) {
+      tag.setAttribute(attr, attributes[attr]);
+    }
+  }
+  if (children) {
+    for (let i in children) {
+      let child = children[i];
+      tag.appendChild(child);
+    }
+  }
+  return tag;
+}
+
+window.a = <div id="a" class="c">
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
